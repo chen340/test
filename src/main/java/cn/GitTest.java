34dd6bd86c34374642f46161d710d7b4c7c10857
czm <czm@qq.com>
2022-06-08 13:16:39 +0800

    master分支第二次提交

--- src/main/java/cn/GitTest.java
+++ src/main/java/cn/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git by hot");
+        System.out.println("master分支第一次修改");
 
 
     }
