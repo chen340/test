7146b48e4a49b8ebd6b5f1c018a1e657ea156867
czm <czm@qq.com>
2022-06-08 13:15:32 +0800

    hot分支第二次提交

--- src/main/java/cn/GitTest.java
+++ src/main/java/cn/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git by hot");
+        System.out.println("hot分支修改");
 
 
     }
