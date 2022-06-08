8469e4c479661f3b4398c0142d16f2bc3a1306e1
czm <czm@qq.com>
2022-06-08 13:46:41 +0800

    master分支第三次提交

--- src/main/java/cn/GitTest.java
+++ src/main/java/cn/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("hello git by hot");
         System.out.println("master分支第一次修改");
         System.out.println("hot分支修改");
+        System.out.println("本地库修改提交到远程库");
 
 
     }
